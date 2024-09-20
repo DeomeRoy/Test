6e97d597ed884dfa67ec57ae62f332a0ce61f5b2
DeomeRoy <deomeroy@gmail.com>
2024-09-20 16:09:17 +0800

    20240920

--- BMI.js
+++ BMI.js
@@ -1,5 +1,7 @@
-var w =50
-var h =160
-var bmi=w/((h/100)**2)
-
-console.log("Halo!!!Your BMI value is "+ bmi)
+function calcBmi(){
+    var h = Number(document.getElementById("h").value);
+    var w = Number(document.getElementById("w").value);
+    var bmi = w/((h/100)**2);
+    document.getElementById("bmi").innerHTML = bmi;
+    console.log("Halo!!!Your BMI value is "+ bmi);
+}
