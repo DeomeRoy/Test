a871aef67bd64cf06acf3182a97430082f5def09
DeomeRoy <deomeroy@gmail.com>
2024-11-29 13:13:16 +0800

    Test

--- Project/Maze/Maze.js
+++ Project/Maze/Maze.js
@@ -1,16 +1,3 @@
-// var MAZE = [
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-//     [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-// ];//1,1  end:8,10
-
 MAZE=[
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
@@ -34,8 +21,8 @@ MAZE=[
     [1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
     //end:20,20
-    
-    //object oriented
+    const Road = 0,Wall = 1,Past = 2;
+
     class Point{
       constructor(_row, _col){
         this.row = _row;
@@ -45,19 +32,20 @@ MAZE=[
         return this.row==end.row && this.col==end.col
       }
     }
+    
     var start= new Point(1,1);//{row:1,col:1}
     var end = new Point(19,19);//[8,10]
     var Stack=[];
     var step = start;
     var rollBack=false;
+
     function go(){
          Stack.push(step);
          
          while(! step.isEnd()){
             MAZE[step.row][step.col]=2;
-            
-            //up
-            if(MAZE[step.row-1][step.col] == 0)
+
+            if(MAZE[step.row-1][step.col] == 0)//up
             {
                if(rollBack){
                 Stack.push(step);
@@ -74,6 +62,7 @@ MAZE=[
                 
                step = new Point(step.row+1, step.col);  
                 Stack.push(step);
+
             }else if(MAZE[step.row][step.col-1] == 0){//left
               if(rollBack){
                 Stack.push(step);
@@ -81,6 +70,7 @@ MAZE=[
                }
                 step = new Point(step.row, step.col-1); 
                 Stack.push(step);
+
             }else if(MAZE[step.row][step.col+1] == 0){//right
               if(rollBack){
                 Stack.push(step);
@@ -88,6 +78,7 @@ MAZE=[
                }
                 step = new Point(step.row, step.col+1);
                 Stack.push(step);
+                
             }else{
                 if(Stack.length>0){
                   step = Stack.pop();
@@ -102,6 +93,4 @@ MAZE=[
          else 
             console.log("No solution!");
     }
-    
-    go()
      
